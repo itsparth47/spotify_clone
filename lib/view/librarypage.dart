2aff4467843be3d34c713d47657ee0a39b026582
Parth Malvi <93420021+itsparth47@users.noreply.github.com>
2023-05-31 09:42:58 +0530

    Update librarypage.dart

--- lib/view/librarypage.dart
+++ lib/view/librarypage.dart
@@ -87,10 +87,167 @@ class _LibraryScreenState extends State<LibraryScreen> {
                       ),
                       itemCount: 6,
                       itemBuilder: (context, index){
-                        return Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                          ),
+                        return InkWell(
+                          onTap: (){
+                            Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                              if(index == 0){
+                                return ArtistScreen(
+                                  title: 'BLACKPINK',
+                                  coverimage: bpcoverimage,
+                                  aboutimage: bpaboutimage,
+                                  songicon: bpsongicon,
+                                  songs: bpsongs,
+                                  popular: bppopular,
+                                  albumicon: bpalbumicon,
+                                  albums: bpalbums,
+                                  albumyear: bpalbumyear,
+                                  featuringimage: bpfeaturingimage,
+                                  featuringnames: bpfeaturingnames,
+                                  artistplaylistimage: bpartistplaylistimage,
+                                  artistplaylistnames: bpartistplaylistnames,
+                                  similarimages: bpsimilarimages,
+                                  similarnames: bpsimilarnames,
+                                  songcount: 5,
+                                  albumcount: 4,
+                                  playlistcount: 6,
+                                  monthlylisteners: bpmonthlylisteners,
+                                  about: bpabout,
+                                  canvas: bpcanvas,
+                                  music: bpmusic,
+                                );
+                              }
+                              else if(index == 1){
+                                return ArtistScreen(
+                                  title: 'Anuv Jain',
+                                  coverimage: anuvcoverimage,
+                                  aboutimage: anuvaboutimage,
+                                  songicon: anuvsongicon,
+                                  songs: anuvsongs,
+                                  popular: anuvpopular,
+                                  albumicon: anuvalbumicon,
+                                  albums: anuvalbums,
+                                  albumyear: anuvalbumyear,
+                                  featuringimage: anuvfeaturingimage,
+                                  featuringnames: anuvfeaturingnames,
+                                  artistplaylistimage: anuvartistplaylistimage,
+                                  artistplaylistnames: anuvartistplaylistnames,
+                                  similarimages: anuvsimilarimages,
+                                  similarnames: anuvsimilarnames,
+                                  songcount: 5,
+                                  albumcount: 4,
+                                  playlistcount: 1,
+                                  monthlylisteners: anuvmonthlylisteners,
+                                  about: anuvabout,
+                                  canvas: anuvcanvas,
+                                  music: anuvmusic,
+                                );
+                              }
+                              else if(index == 2){
+                                return ArtistScreen(
+                                  title: 'Taylor Swift',
+                                  coverimage: taylorcoverimage,
+                                  aboutimage: tayloraboutimage,
+                                  songicon: taylorsongicon,
+                                  songs: taylorsongs,
+                                  popular: taylorpopular,
+                                  albumicon: tayloralbumicon,
+                                  albums: tayloralbums,
+                                  albumyear: tayloralbumyear,
+                                  featuringimage: taylorfeaturingimage,
+                                  featuringnames: taylorfeaturingnames,
+                                  artistplaylistimage: taylorartistplaylistimage,
+                                  artistplaylistnames: taylorartistplaylistnames,
+                                  similarimages: taylorsimilarimages,
+                                  similarnames: taylorsimilarnames,
+                                  songcount: 5,
+                                  albumcount: 3,
+                                  playlistcount: 3,
+                                  monthlylisteners: taylormonthlylisteners,
+                                  about: taylorabout,
+                                  canvas: taylorcanvas,
+                                  music: taylormusic,
+                                );
+                              }
+                              else if(index == 3){
+                                return ArtistScreen(
+                                  title: 'LISA',
+                                  coverimage: lisacoverimage,
+                                  aboutimage: lisaaboutimage,
+                                  songicon: lisasongicon,
+                                  songs: lisasongs,
+                                  popular: lisapopular,
+                                  albumicon: lisaalbumicon,
+                                  albums: lisaalbums,
+                                  albumyear: lisaalbumyear,
+                                  featuringimage: lisafeaturingimage,
+                                  featuringnames: lisafeaturingnames,
+                                  artistplaylistimage: lisaartistplaylistimage,
+                                  artistplaylistnames: lisaartistplaylistnames,
+                                  similarimages: lisasimilarimages,
+                                  similarnames: lisasimilarnames,
+                                  songcount: 4,
+                                  albumcount: 2,
+                                  playlistcount: 3,
+                                  monthlylisteners: lisamonthlylisteners,
+                                  about: lisaabout,
+                                  canvas: lisacanvas,
+                                  music: lisamusic,
+                                );
+                              }
+                              else if(index == 4){
+                                return ArtistScreen(
+                                  title: 'Doja Cat',
+                                  coverimage: dojacoverimage,
+                                  aboutimage: dojaaboutimage,
+                                  songicon: dojasongicon,
+                                  songs: dojasongs,
+                                  popular: dojapopular,
+                                  albumicon: dojaalbumicon,
+                                  albums: dojaalbums,
+                                  albumyear: dojaalbumyear,
+                                  featuringimage: dojafeaturingimage,
+                                  featuringnames: dojafeaturingnames,
+                                  artistplaylistimage: dojaartistplaylistimage,
+                                  artistplaylistnames: dojaartistplaylistnames,
+                                  similarimages: dojasimilarimages,
+                                  similarnames: dojasimilarnames,
+                                  songcount: 5,
+                                  albumcount: 4,
+                                  playlistcount: 2,
+                                  monthlylisteners: dojamonthlylisteners,
+                                  about: dojaabout,
+                                  canvas: dojacanvas,
+                                  music: dojamusic,
+                                );
+                              }
+                              else{
+                                return ArtistScreen(
+                                  title: 'Kali Uchis',
+                                  coverimage: kalicoverimage,
+                                  aboutimage: kaliaboutimage,
+                                  songicon: kalisongicon,
+                                  songs: kalisongs,
+                                  popular: kalipopular,
+                                  albumicon: kalialbumicon,
+                                  albums: kalialbums,
+                                  albumyear: kalialbumyear,
+                                  featuringimage: kalifeaturingimage,
+                                  featuringnames: kalifeaturingnames,
+                                  artistplaylistimage: kaliartistplaylistimage,
+                                  artistplaylistnames: kaliartistplaylistnames,
+                                  similarimages: kalisimilarimages,
+                                  similarnames: kalisimilarnames,
+                                  songcount: 5,
+                                  albumcount: 4,
+                                  playlistcount: 4,
+                                  monthlylisteners: kalimonthlylisteners,
+                                  about: kaliabout,
+                                  canvas: kalicanvas,
+                                  music: kalimusic,
+                                );
+                              }
+                            } ));
+                          },
                           child: Row(
                             children: [
                               SizedBox(
